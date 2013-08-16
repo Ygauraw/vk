4d24a49d1dc69879f990ada0d8587c2fe121142d
Gark <artem.garkusha@gmail.com>
2013-08-16 08:03:33 +0300

    1.12

--- vk/src/com/gark/vk/network/ApiHelper.java
+++ vk/src/com/gark/vk/network/ApiHelper.java
@@ -22,23 +22,49 @@ public class ApiHelper extends BaseApiHelper {
     public static final int POPULAR_TOKEN = 1;
     public static final int AUDIO_TOKEN = 2;
     public static final int VIDEO_TOKEN = 3;
-    public static final int COUNT = 20;
+    public static final int COUNT = 40;
     private Random random;
     private int englishResultsFlag = 0;
     private Context context;
     //    private static final String ACCESS_TOKEN = "37176714256e377a408a2478728e7c06fc0586dcff56aed28d6b1dd1e3598f3af7e7a9268bb1b3ffd2c6d";
     private static final String[] ACCESS_TOKENS = new String[]{
-            "37176714256e377a408a2478728e7c06fc0586dcff56aed28d6b1dd1e3598f3af7e7a9268bb1b3ffd2c6d", "74e37c7cae5fe2f9d48f5533686cd0979f4e44fd070979ae9dd5d26b42b591161fdefd95396850d0ffde9",
-            "c82cb8591d7cd186916028bb29b3faf289999328ac5482d2a998747e3f116cf16201c3d8be6e9c328c3e5", "4057a57148daca8c3d5fc6e4e10bf37b42ea67bcb0bc059c6fbc74766c35b027d96e95c465d21792932ed",
-            "a21756528ec71dec8b016b7975b470c708b54535d1d068f130641790795f2a05243f478a6898c82481e91", "d868d0c17e551f0d61f01b0e2cdcbf31908531227dd036e43cb75ce33e66ce3a2eade69a53437dec6272a",
-            "2b51f6a4e797603ddbe85cdef1de20f872ffe229fc9de87478efe037148793689c320d3b4a5226b0bb220", "03328309b844c9cc0b6ad716238ac8d583562d0dccc56ff2fcd755913bf021c4ca5d64b164ada2869ada1",
-            "a10b720def064f31ffd3e06e8966aad4faac465f0a4c6be4c8576e3354c008bac6cf7c38d8fdf8a530b9c", "6224917185ff5ae27ce3da74fbceaf7768ebb9df00d46de6aa5ac7f6e7296030d63ce3830f05850429b9b",
-            "25829f0bb02a0c8cc32905313d6c3a4d7c0025f9423f47ad453edf54fe8b0b258011158407515a71ee934", "f900693cbd1c904c332b28c6bb742101f332d4eb851054fde761329025cdebfab54c46ad4c4fb05cfbc9e",
-            "30a50334af0b2bc6a4c8833e5a81da0fc1064e628363e8b00597780335e1674d97ad94cf2a5c78443ff57", "4a0494f389ac795e344e6e383e11f7d0394e1922b46b747583dc022432650045337f19486648d230dcf74",
-            "170653b9b04dd3de60ae743d9ccbed40c5dcd4a2bb57d7beccb629a2f02354ae3afa095be2cf7e56ffda3", "608f6518a6b17218e08a47918393833880795488111a0013a2c0dabbae5c851baf04efab1b6ad0c6f2b5a"
+//            "37176714256e377a408a2478728e7c06fc0586dcff56aed28d6b1dd1e3598f3af7e7a9268bb1b3ffd2c6d", "74e37c7cae5fe2f9d48f5533686cd0979f4e44fd070979ae9dd5d26b42b591161fdefd95396850d0ffde9",
+//            "c82cb8591d7cd186916028bb29b3faf289999328ac5482d2a998747e3f116cf16201c3d8be6e9c328c3e5", "4057a57148daca8c3d5fc6e4e10bf37b42ea67bcb0bc059c6fbc74766c35b027d96e95c465d21792932ed",
+//            "a21756528ec71dec8b016b7975b470c708b54535d1d068f130641790795f2a05243f478a6898c82481e91", "d868d0c17e551f0d61f01b0e2cdcbf31908531227dd036e43cb75ce33e66ce3a2eade69a53437dec6272a",
+//            "2b51f6a4e797603ddbe85cdef1de20f872ffe229fc9de87478efe037148793689c320d3b4a5226b0bb220", "03328309b844c9cc0b6ad716238ac8d583562d0dccc56ff2fcd755913bf021c4ca5d64b164ada2869ada1",
+//            "a10b720def064f31ffd3e06e8966aad4faac465f0a4c6be4c8576e3354c008bac6cf7c38d8fdf8a530b9c", "6224917185ff5ae27ce3da74fbceaf7768ebb9df00d46de6aa5ac7f6e7296030d63ce3830f05850429b9b",
+//            "25829f0bb02a0c8cc32905313d6c3a4d7c0025f9423f47ad453edf54fe8b0b258011158407515a71ee934", "f900693cbd1c904c332b28c6bb742101f332d4eb851054fde761329025cdebfab54c46ad4c4fb05cfbc9e",
+//            "30a50334af0b2bc6a4c8833e5a81da0fc1064e628363e8b00597780335e1674d97ad94cf2a5c78443ff57", "4a0494f389ac795e344e6e383e11f7d0394e1922b46b747583dc022432650045337f19486648d230dcf74",
+//            "170653b9b04dd3de60ae743d9ccbed40c5dcd4a2bb57d7beccb629a2f02354ae3afa095be2cf7e56ffda3", "608f6518a6b17218e08a47918393833880795488111a0013a2c0dabbae5c851baf04efab1b6ad0c6f2b5a",
+
+            "d209831e33938b0a39a39bf941795bc7e2c5e364f6d4b3579f470cc15c70bd14c95012353a02203e6d950",
+            "7c4857938de77bb0549f085276d57e568aa34fde33e0f5256a7a046322465df25c7e9247479f7b3054590",
+            "9a26fa7b16069d62472df8c4112684e549b54a7ad6438aba1d20559dfb103d0228d443716ba30f9967037",
+            "ed3a3c7b2e54b7d9ec68cf350dbc937ec3e3e43c14001f40b831146204154f2f6c212dce8d78a0257e2da",
+            "7be005281d1e21e317e2f04cf890acd09ebbeee558a4e23dd1e8d4da8e888fe151caa251a8cacfcd2965d",
+
+            "7fcb578cc3128dc96579db09e6f088347c2512ac1fd1b5c32931cb6054318203344c06a9b4d629dce0caf",
+            "2d102883c5631bb7ffb95bf0dccfd01ce89e0e4a4c028d479ad5fd517b8367985f017f8c20c995e8a169b",
+            "19ae26a756029be62ca5603854d414d624ae91c748eb0e49f81d8eff088ff09e956f2664d7ec158845920",
+            "8cbcf5cec7d0a6651b85d9359ff8f43e9db5cfd9590528999a1d4c3d1836985125e0985a6e33c4d8fe9e1",
+            "124cf54664e55c41f75f907222fcc8ec6ee6e6cf218af8c4dcf5e3ea4e47077fb9d06c7f7bec37144306e"
+
+
+
+
+
+
+
+
 
     };
+// Lena
+    //    "https://oauth.vk.com/authorize?client_id=3824524&redirect_uri=http://api.vk.com/blank.html&display=page&v=5.0&scope=audio,video,offline&response_type=token"
 
+//    "https://oauth.vk.com/authorize?client_id=3010909&redirect_uri=http://api.vk.com/blank.html&display=page&v=5.0&scope=audio,video,offline&response_type=token"
+
+
+    // my
 // "https://oauth.vk.com/authorize?client_id=3746605&redirect_uri=http://api.vk.com/blank.html&display=page&v=5.0&scope=audio,video,offline&response_type=token";
 //	https://api.vk.com/method/audio.search.json?q=AC/DC%20-%20Highway%20to%20Hell&access_token=37176714256e377a408a2478728e7c06fc0586dcff56aed28d6b1dd1e3598f3af7e7a9268bb1b3ffd2c6d
 
